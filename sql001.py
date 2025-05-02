ee842b935a89d093f9b5f040c5805208fa545cd3
Kurihararyder <kurihararyder@gmail.com>
2025-05-02 15:17:51 +0800

    Add files via upload

---
+++ sql001.py
@@ -0,0 +1,52 @@
+import sqlite3
+conn = sqlite3.connect("Hospital.db")
+cursor = conn.cursor()
+    
+    # 病患資料表
+cursor.execute("""CREATE TABLE IF NOT EXISTS patients (
+        patient_id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        birthday TEXT NOT NULL
+    )"""
+)
+    
+    # 檢驗數值
+cursor.execute("""CREATE TABLE IF NOT EXISTS test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    patient_id TEXT NOT NULL,
+    Glucose REAL,
+    HbA1c REAL,
+    Glupc REAL,
+    Alb REAL,
+    TP REAL,
+    ASTGOT REAL,
+    ASTGPT REAL,
+    DBil REAL,
+    ALP REAL,
+    TBil REAL,
+    UN REAL,
+    CRE REAL,
+    UA REAL,
+    TCHO REAL,
+    LDLC REAL,
+    HDLC REAL,
+    TG REAL,
+    Hb REAL,
+    Hct REAL,
+    PLT REAL,
+    WBC REAL,
+    RBC REAL,
+    hsCRP REAL,
+    Alpha REAL,
+    CEA REAL,
+    CA125 REAL,
+    CA199 REAL,
+    test_id TEXT UNIQUE NOT NULL,
+    FOREIGN KEY(patient_id) REFERENCES patients(patient_id)
+)"""
+)
+
+    
+conn.commit()
+conn.close()
+print("資料庫新增完成")
